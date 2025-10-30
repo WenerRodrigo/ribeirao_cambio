af10765bd4c96a4d10be01cdf84259aabc78509d
Wener Rodrigo <wener19.wr@gmail.com>
2025-10-29 21:30:36 -0300

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,15 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    'text-turquoise',
+    'hover:text-turquoise'
+  ],
   theme: {
     extend: {
       colors: {
-        turquoise: 'rgb(52, 185, 194)'
-      }
+        turquoise: 'rgb(52, 185, 194)',
+      },
     },
   },
   plugins: [],
